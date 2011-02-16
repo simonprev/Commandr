74a8e2115a51ee82d037ac5c4fdfe438b29bb76f
Simon Prévost <simonprevost@gmail.com>
2011-02-16 16:45:50 -0500

    added json to load more publications

--- commandr.rb
+++ commandr.rb
@@ -1,14 +1,18 @@
 require 'bundler'
 Bundler.require :default
 
-require 'model.rb'
-
-
 DataMapper::Pagination.defaults[:size] = 5
 DataMapper::Pagination.defaults[:per_page] = 5
 
 class App < Sinatra::Base
+
+  require 'model'
+  require 'digest/sha1'
+  require 'salt' #Algorithm super secret indéchiffrable
+
   use Rack::MethodOverride
+
+
   enable :sessions
   enable :static
   puts "****************************"
@@ -20,39 +24,36 @@ class App < Sinatra::Base
     if session["username"]
       redirect '/feed'
     else
-      haml :index
+      haml :"user/login"
     end
 
   end #}}}
 
-  # Dernière activités
+  ############# AFFICHAGE DES PUBLICATIONS ###############
+  # Dernière activités pour user connecté
   get '/feed' do #{{{
     if session["username"]
-      set_titre "Dernières Activités"
+      @titre="Dernières Activités"
       set_self session["username"]
 
-      @friends=[]
-      @self.friends.each do |user|
-        @friends << user.id
-      end
-
-      @friends << @self.id
 
       @count=Work.all(:user_id=>@friends).count
       set_pagination @count,params[:page]
 
-      @feed=Work.all(:order=>:updated_at.desc, :user_id=>@friends).page @page
+      @feed=Work.all(:order=>:updated_at.desc, :user_id=>[@self.follows,@self.id]).page @page
 
-      haml :code
+      return works_json @feed if request.xhr?
+
+      haml :'publication/index'
 
     else
       redirect '/'
     end
   end #}}}
 
-  # Liste de codes
-  get '/code' do #{{{
-    set_titre "Code"
+  #Toutes les publications
+  get '/publication' do #{{{
+    @titre="Publications"
     set_self session["username"]
 
     @count=Work.all.count
@@ -71,32 +72,91 @@ class App < Sinatra::Base
       @feed=Work.all().page @page, :order=>[:count_favorite.desc,:updated_at.desc]
     end
 
-    haml :code
+    return works_json @feed if request.xhr?
+
+    haml :'publication/index'
 
   end #}}}
 
-  # Nouveau Code
+
+  # Liste de codes
+  get '/code' do #{{{
+    @titre="Codes"
+    set_self session["username"]
+
+    @count=Work.all(:type.not=>"image").count
+    set_pagination @count,params[:page]
+
+    @tri=params[:tri] || "recent"
+
+    if @tri=="recent" || !@tri
+      #sortby Date de création!
+      @feed=Work.all(:type.not=>"image").page @page, :order=>:updated_at.desc
+    elsif @tri=="populaire"
+      #sortby Views!
+      @feed=Work.all(:type.not=>"image").page @page, :order=>[:views.desc,:updated_at.desc]
+    elsif @tri=="favoris"
+      #sortby Favorited!
+      @feed=Work.all(:type.not=>"image").page @page, :order=>[:count_favorite.desc,:updated_at.desc]
+    end
+
+    return works_json @feed if request.xhr?
+
+    haml :'publication/index'
+
+
+  end #}}}
+
+  # Liste des images
+  get '/image' do #{{{
+    @titre="Images"
+    set_self session["username"]
+
+    @count=Work.all(:type=>"image").count
+    set_pagination @count,params[:page]
+
+    @tri=params[:tri] || "recent"
+
+    if @tri=="recent" || !@tri
+      #sortby Date de création!
+      @feed=Work.all(:type=>"image").page @page, :order=>:updated_at.desc
+    elsif @tri=="populaire"
+      #sortby Views!
+      @feed=Work.all(:type=>"image").page @page, :order=>[:views.desc,:updated_at.desc]
+    elsif @tri=="favoris"
+      #sortby Favorited!
+      @feed=Work.all(:type=>"image").page @page, :order=>[:count_favorite.desc,:updated_at.desc]
+    end
+
+    return works_json @feed if request.xhr?
+
+    haml :'publication/index'
+
+  end #}}}
+
+  # Nouveau Publication
   get '/new' do #{{{
 
     if session["username"]
-      haml :new
+      haml :'publication/new'
     else
       redirect '/login'
     end
 
   end #}}}
 
-    get '/new_code' do #{{{
+  #Nouveau Code
+  get '/new_code' do #{{{
 
     if session["username"]
-      haml :new_code
+      haml :'publication/new_code'
     else
       redirect '/login'
     end
 
   end #}}}
 
-  #save new
+  #Save Code
   post '/new_code' do #{{{
     @self = User.first(:username=>session["username"])
 
@@ -112,22 +172,23 @@ class App < Sinatra::Base
     if @work.save
       redirect "/#{@work.id}"
     else
-      haml :new
+      haml :'publciation/new_code'
     end
 
   end #}}}
 
-  # Nouveau Code
+  # Nouvelle image
   get '/new_image' do #{{{
 
     if session["username"]
-      haml :new_image
+      haml :'publication/new_image'
     else
       redirect '/login'
     end
 
   end #}}}
 
+  #Save image
   post '/new_image' do
     @work = Work.new(:titre => params[:work_title],
                      :body => "Aucun code pour une image",
@@ -137,6 +198,7 @@ class App < Sinatra::Base
                      :user_id=>session["id"]
                      )
 
+    #UPLOAD D'IMAGE EN DÉVELOPPEMENT (aucunement sécuritaire)
     if @work.save && params[:file] && (tmpfile = params[:file][:tempfile]) && (name = params[:file][:filename])
       File.open(File.join(Dir.pwd,"public/files", @work.id.to_s), "wb") { |f| f.write(tmpfile.read) }
       redirect "/#{@work.id}"
@@ -144,13 +206,12 @@ class App < Sinatra::Base
       unless params[:file] && (tmpfile = params[:file][:tempfile]) && (name = params[:file][:filename])
         @work.errors.add :fichier,"Vous devez choisir un fichier"
       end
-      haml :new_image
+      haml :'publication/new_image'
     end
 
   end
 
-  ## Auteur related routes ##
-
+  ################ OPTIONS DE USER ##################
   # List Auteurs
   get '/user' do #{{{
 
@@ -158,27 +219,48 @@ class App < Sinatra::Base
     set_pagination @count,params[:page]
 
     @users=User.all().page @page, :order=>:updated_at.desc
-    haml :all_user
+    haml :'user/index'
 
   end #}}}
 
+  #Création d'un user
+  get '/user/new' do
+    haml :'user/new'
+  end
+
+  post '/user/new' do
+
+    @user=User.new :nom=>params[:nom], 
+                   :prenom=>params[:prenom], 
+                   :username=>params[:username],
+                   :site_web=>params[:site_web],
+                   :desc=>params[:desc],
+                   :twitter_username=>params[:twitter_username]
+
+    if params[:password].length >= 4 && params[:password].length <= 20
+      @password=Digest::SHA1.hexdigest(params[:password])
+      @salted_password=salt params[:username], @password
+      @user.password=@salted_password
+    else
+      @user.password=params[:password]
+      @mauvais_password=true
+    end
+
+    if @user.save
+      session["username"]=@user.username
+      session["id"]=@user.id
+      redirect "/user/#{params[:username]}"
+    else
+      @user.errors.add(:password,"Le mot de passe doit contenir entre 4 et 20 caractères") if @mauvais_password
+      haml :"user/new"
+    end
+  end
+
   # Profil User
   get '/user/:user' do #{{{
-    set_titre "Code"
+    @titre="#{params[:user]}"
     set_self session["username"]
     @user=User.first(:username=>params[:user])
-    @is_friend=Friendship.between @self,@user
-
-    @follow=[]
-    @user.friends.each do |i|
-      @follow << i
-    end
-
-    @all_followers=Friendship.all(:target=>@user)
-    @followers=[]
-    @all_followers.each do |i|
-      @followers << i.source
-    end
 
     @count=Work.all(:user_id=>@user.id).count
     set_pagination @count,params[:page]
@@ -196,9 +278,7 @@ class App < Sinatra::Base
       @feed=Work.all(:user_id=>@user.id).page @page, :order=>[:count_favorite.desc,:created_at.desc]
     end
 
-
-
-    haml :user
+    haml :'user/show'
 
   end #}}}
 
@@ -207,23 +287,97 @@ class App < Sinatra::Base
     set_self session["username"]
     @user=User.first(:username=>params[:user])
 
-    if !@self.friends.include? @user
-      @friends=Friendship.new(:source => @self, :target=> @user)
-      @friends.save
+    if !@self.follows.include? @user
+      @f=Fellowship.new(:source => @self, :target=> @user)
+      @f.save
     else
-      @friends=Friendship.first(:source => @self, :target=> @user)
-      @friends.destroy
+      @f=Fellowship.first(:source => @self, :target=> @user)
+      @f.destroy
     end
 
     redirect "/user/#{params[:user]}"
 
   end #}}}
 
+  get '/parametres' do
+    if session["username"]
+      @user=User.first(:username=>session["username"])
+      haml :"user/edit"
+    else
+      redirect "/login"
+    end
+  end
 
+  post '/parametres' do
+
+    @user=User.first(:username=>session["username"])
+    p @user.created_at
+    if @user.update :nom=>params[:nom],
+                    :prenom=>params[:prenom],
+                    :site_web=>params[:site_web],
+                    :twitter_username=>params[:twitter_username]
+
+
+      redirect "/user/#{@user.username}"
+    else
+      p @user.errors
+      haml :"user/edit"
+    end
+  end
+
+  ##### LOGIN ET LOGOUT ##### 
+  # Page Login
+  get '/login' do #{{{
+    @erreur=" "
+    haml :'user/login'
+  end #}}}
+
+  # Verification Login /AJAX/
+  post '/login' do #{{{
+
+    @user=User.first(:username=>params[:username])
+    @login=false
+    @password=Digest::SHA1.hexdigest(params[:password])
+    if salt(params[:username],@password)==@user.password
+      session["username"]||=@user.username
+      session["id"]||=@user.id
+      @login=@user.username
+      p session["username"]
+    else
+      @login=false
+    end
+
+    if @login
+      if request.xhr? #AAAAJJJJAAAAXXXXX
+        return "true"
+      else
+        redirect "/user/#{@login}"
+      end
+    else
+      if request.xhr?
+        return "false"
+      else
+        @erreur="Les informations de connexion sont invalides"
+        haml :'user/login'
+      end
+    end
+
+  end #}}}
+
+  # Logout User
+  get '/logout' do #{{{
+
+    session["username"]=nil
+    session["id"]=nil
+    redirect "/login"
+
+  end #}}}
+
+  ################ OPTIONS DE TYPE ##################
   # List Langages
   get '/type' do
 
-    haml :all_type
+    haml :'type/index'
 
   end
 
@@ -231,7 +385,7 @@ class App < Sinatra::Base
   get '/type/:type' do
 
     set_self session["username"]
-    set_titre "Code - #{params[:type].capitalize}"
+    @titre="Publications - #{params[:type].capitalize}"
 
     @count=Work.all(:type=>params[:type]).count
     set_pagination @count,params[:page]
@@ -249,60 +403,13 @@ class App < Sinatra::Base
       @feed=Work.all(:type=>params[:type]).page @page, :order=>[:count_favorite.desc,:created_at.desc]
     end
 
-    haml :code
+    haml :'publication/index'
 
   end
 
-  # Page Login
-  get '/login' do #{{{
-    @erreur=" "
-    haml :login
-  end #}}}
 
-  # Verification Login /AJAX/
-  post '/login' do #{{{
 
-    @users=User.all
-    @login=false
-    @users.each do |user|
-      if params[:username]==user.username
-        if params[:password]==user.password
-          session["username"]||=user.username
-          session["id"]||=user.id
-          @login=user.username
-        else
-          @login=false
-        end
-      end
-    end
-
-    if @login
-      if request.xhr? #AAAAJJJJAAAAXXXXX
-        return "true"
-      else
-        redirect "/user/#{@login}"
-      end
-    else
-      if request.xhr?
-        return "false"
-      else
-        @erreur="Les informations de connexion sont invalides"
-        haml :login
-      end
-    end
-
-  end #}}}
-
-  # Logout User
-  get '/logout' do #{{{
-
-    session["username"]=nil
-    session["id"]=nil
-    redirect "/login"
-
-  end #}}}
-
-## ID related routes ##
+  ########## OPTION DE PUBLICATION ############
   get %r{^\/([0-9]{0,4})$} do |id|
 
     set_self session["username"]
@@ -321,9 +428,9 @@ class App < Sinatra::Base
         @code.update!(:views=>@code.views+1)
       end
 
-      haml :show
+      haml :'publication/show'
     else
-      redirect "/sorry_not_found"
+      redirect "/cannot_find_what_you_are_searching_for_sorry"
     end
 
   end #}}}
@@ -344,15 +451,8 @@ class App < Sinatra::Base
       Favorite.create(:user=>@self,:work=>@code)
     end
 
-    if !request.xhr?
-      redirect "/#{params[:id]}"
-    else
-      if @is_favorite
-        return "false"
-      else
-        return "true"
-      end
-    end
+    redirect "/#{params[:id]}"
+
 
   end #}}}
 
@@ -371,9 +471,8 @@ class App < Sinatra::Base
 
 
 
-  delete '/:id/destroy_it' do #{{{
+  delete '/delete/:id' do #{{{
 
-    puts "destroy #{params[:id]}"
     @code=Work.get(params[:id])
 
     if @code.destroy
@@ -385,11 +484,11 @@ class App < Sinatra::Base
   end #}}}
 
   # Page modification d'un code
-  get '/:id/modify' do #{{{
+  get '/edit/:id' do #{{{
 
     @code=Work.get(params[:id])
     if session["id"]==@code.user_id
-      haml :modify
+      haml :'publication/edit'
     else
       redirect "/#{params[:id]}"
     end
@@ -397,9 +496,9 @@ class App < Sinatra::Base
   end #}}}
 
   # Enregistrer les modifications
-  put "/:id/modify" do #{{{
+  put "/edit/:id" do #{{{
     @error=[]
-    @code=Work.get(params[:id])
+    @code=Work.first(params[:id])
     if params[:work_title]==""
       @error.push "Le titre ne doit pas être vide"
     end
@@ -423,13 +522,11 @@ class App < Sinatra::Base
   end
 
 
-  def is_self person1,person2
-    if person1==person2
-      return "by_user"
-    else
-      return nil
-    end
-  end
+  def works_json feed # {{{
+    {:html => haml(:_liste, :layout => false, :locals => { :feed => feed }),
+     :pagination => @total_page,
+    }.to_json
+  end # }}}
 
   def set_pagination count,page
 
@@ -439,53 +536,66 @@ class App < Sinatra::Base
 
   end
 
-  def set_titre titre
-    @titre=titre
-  end
-
   def set_self username
     @self=User.first(:fields=>[:id,:username],:username=>username)
   end
 
-  def set_tags
-    cpt=0
-    request.path.each_char do |i|
-      cpt=cpt+1 if i=="/"
-    end
 
-    if cpt==2
-      return '<script type="text/javascript" src="../js/global.js"></script>'
-      #<link href="../css/global.css" rel="stylesheet" type="text/css"/>'
-    else
-      return '<script type="text/javascript" src="js/global.js"></script>'
-      #<link href="../css/global.css" rel="stylesheet" type="text/css"/>'
-    end
-  end
 
-  def distance_of_time_in_words(from_time)
-      to_time=Time.now
-      distance_in_minutes = (((to_time - from_time).abs)/60).round
-      distance_in_seconds = ((to_time - from_time).abs).round
-      case distance_in_minutes
-          when 0..1
-              case distance_in_seconds
-                  when 0..5   then 'less than 5 seconds ago'
-                  when 6..10  then 'less than 10 seconds ago'
-                  when 11..20 then 'less than 20 seconds ago'
-                  when 21..40 then 'half a minute ago'
-                  when 41..59 then 'less than a minute ago'
-                  else             '1 minute ago'
-              end
-
-              when 2..45           then "#{distance_in_minutes} minutes ago"
-              when 46..90          then 'about 1 hour ago'
-              when 90..1440        then "about #{(distance_in_minutes / 60).round} hours ago"
-              when 1441..2880      then '1 day ago'
-              when 2881..525961    then Time.at(from_time).strftime("%e %b")
-          else                      Time.at(from_time).strftime("%e %B %Y")
+  ##### HELPERS #####
+  helpers do 
+    def set_tags
+      cpt=0
+      request.path.each_char do |i|
+        cpt=cpt+1 if i=="/"
       end
-  end
 
+      if cpt==2
+        return '<script type="text/javascript" src="../js/global.js"></script>'
+        #<link href="../css/global.css" rel="stylesheet" type="text/css"/>'
+      else
+        return '<script type="text/javascript" src="js/global.js"></script>'
+        #<link href="../css/global.css" rel="stylesheet" type="text/css"/>'
+      end
+    end
+   
+    def distance_of_time_in_words(from_time)
+        to_time=Time.now
+        distance_in_minutes = (((to_time - from_time).abs)/60).round
+        distance_in_seconds = ((to_time - from_time).abs).round
+        case distance_in_minutes
+            when 0..1
+                case distance_in_seconds
+                    when 0..5   then 'less than 5 seconds ago'
+                    when 6..10  then 'less than 10 seconds ago'
+                    when 11..20 then 'less than 20 seconds ago'
+                    when 21..40 then 'half a minute ago'
+                    when 41..59 then 'less than a minute ago'
+                    else             '1 minute ago'
+                end
+
+                when 2..45           then "#{distance_in_minutes} minutes ago"
+                when 46..90          then 'about 1 hour ago'
+                when 90..1440        then "about #{(distance_in_minutes / 60).round} hours ago"
+                when 1441..2880      then '1 day ago'
+                when 2881..525961    then Time.at(from_time).strftime("%e %b")
+            else                      Time.at(from_time).strftime("%e %B %Y")
+        end
+    end
+   
+    def is_favorite user,work
+      if Favorite.first(:fields=>[:work_id],:user=> user, :work=>work)
+        return "favori"
+      else
+        return nil
+      end
+    end
+    
+    def form_erreur champ
+      @user.errors[champ] if @user && @user.errors
+    end
+
+  end
 end
 
 
